51eb20478134bad453ea664a7e01ff8eee5a4422
INTRANET\mertenma <mertenma@123u.com>
2018-12-05 20:29:28 +0800

    Edtior Tools Hierarchy Set

---
+++ MFrameWork/Assets/Tools/Editor/HierarchySet/HierarchyGameObjectEditor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[InitializeOnLoad]
+public class HierarchyGameObjectEditor
+{
+    private static bool isShowActiveToogle = false;
+
+    [MenuItem("MSimpleTools/HierarchySet(层次界面Toggle设置)/GameObject Active Toggle Close")]
+    public static void CloseToggle()
+    {
+        PlayerPrefs.SetString("GameObjectActiveToggle", "false");
+    }
+
+    [MenuItem("MSimpleTools/HierarchySet(层次界面Toggle设置)/GameObject Active Toggle Open")]
+    public static void OpenToggle()
+    {
+        PlayerPrefs.SetString("GameObjectActiveToggle", "true");
+    }
+
+    static HierarchyGameObjectEditor()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += DrawHierarchyComponent;
+    }
+
+    private static void DrawHierarchyComponent(int instanceID, Rect selectionRect)
+    {
+        GameObject go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+        if (go == null)
+            return;
+        if (PlayerPrefs.GetString("GameObjectActiveToggle") == "false")
+            return;
+        Rect rectCheck = new Rect(selectionRect);
+        rectCheck.x += rectCheck.width - 20;
+        rectCheck.width = 20;
+        go.SetActive(GUI.Toggle(rectCheck, go.activeSelf, string.Empty));
+    }
+}
