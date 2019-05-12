99554314771d3948695c499f8252414a42ee1e87
Max1993 <1098183806@qq.com>
2019-05-12 16:36:41 +0800

    Add MNetworkManager but do nothing

---
+++ MFrameWork/Assets/Scripts/Manager/MNetWorkManager.cs
@@ -0,0 +1,26 @@
+﻿// **************************************
+//
+// 文件名(NewMonoBehaviour.cs):
+// 功能描述("Autor Please Edit"):
+// 作者(Max1993):
+// 日期(2019/5/12  16:21):
+//
+// **************************************
+//
+using UnityEngine;
+using System.Collections;
+
+public class NewMonoBehaviour : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
